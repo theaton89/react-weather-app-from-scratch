7beb958f7d8cb5ace5cb4c095971aaaefea2cbe9
theaton89 <taylornheaton@gmail.com>
2021-05-25 18:37:59 -0500

    Added source code links and created border with basic weather info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      This project was coded by{" "}
-      <a
-        href="https://www.youtube.com/channel/UC0rQDkXDNeidKnUafldmpdA"
-        target="_blank"
-      >
-        Taylor Heaton
-      </a>{" "}
-      and is{" "}
-      <a
-        href="https://github.com/theaton89/weather-app-from-scratch"
-        target="_blank"
-      >
-        open-sourced on GitHub
-      </a>
-      .<button>Hello</button>
+      <Weather />
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://www.youtube.com/channel/UC0rQDkXDNeidKnUafldmpdA"
+          target="_blank"
+        >
+          Taylor Heaton
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/theaton89/weather-app-from-scratch"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </p>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>May 25</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="sunny"
+          />
+          80°F
+        </div>
+      </div>
+    </div>
+  );
+}
