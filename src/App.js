e85a3a8953379b39ef9b219b2ca549f8a389b3f7
theaton89 <taylornheaton@gmail.com>
2021-05-25 18:19:49 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Working</p>
+      <h1>Weather App</h1>
+      This project was coded by{" "}
+      <a
+        href="https://www.youtube.com/channel/UC0rQDkXDNeidKnUafldmpdA"
+        target="_blank"
+      >
+        Taylor Heaton
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/theaton89/weather-app-from-scratch"
+        target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+      .<button>Hello</button>
     </div>
   );
 }
